9662518f51a9348e47e31686d40b1aa2726d5cfc
Joshua Engels <joshuaengels@yahoo.com>
2016-11-23 15:56:47 -0500

    Base for minibot teams to use

---
+++ MinibotCode/src/main/Robot.java
@@ -0,0 +1,77 @@
+package main;
+
+import arduinoControl.Servo;
+
+/**
+ * Our jank code is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot{	
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit() {
+
+	}
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	public void autonomousInit() {
+
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+
+	}
+
+	public void teleopInit(){
+		Servo s = new Servo(4);
+	}
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+
+	}
+
+	public static void main(String[] args){
+		Robot r = new Robot();
+		r.robotInit();
+		boolean auton = false;
+		if(auton){
+			r.autonomousInit();
+			while(true){
+				r.autonomousPeriodic();
+			}
+		}
+		else{
+			r.teleopInit();
+			while(true){
+				r.teleopPeriodic();
+			}
+		}
+	}
+}
+
+
