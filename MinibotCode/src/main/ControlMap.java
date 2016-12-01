d3428a5baf1821c7356b38c63c85530cbb9ee3bd
Helena Buarque de Macedo <princessgleek@ymail.com>
2016-12-01 16:09:52 -0500

    control map

---
+++ MinibotCode/src/main/ControlMap.java
@@ -0,0 +1,20 @@
+package main;
+
+public class ControlMap {
+		//Driver Controls
+		protected static final int ax_WHEEL_AXIS=2;
+		protected static final int ax_THROTTLE_AXIS=1;
+
+		//Turn Controls
+		protected static final int btn_90_DEGREE=1;//a
+		protected static final int btn_45_DEGREE=2;//b
+		protected static final int btn_180_DEGREE=3;//c
+		protected static final int btn_360_DEGREE=4;//d
+		
+		//Button Controls
+		protected static final int btn_UP=1;
+		protected static final int btn_DOWN=2;
+		protected static final int btn_LEFT=3;
+		protected static final int btn_RIGHT=4;
+
+}
