499e328e46dbf13a2c685ded3cbb9f6222bb4078
Jonathan <Jonathan@10.208.73.31>
2016-12-08 15:31:11 -0500

    minor spelling changes

--- MinibotCode/src/main/ControlMap.java
+++ MinibotCode/src/main/ControlMap.java
@@ -14,9 +14,9 @@ public class ControlMap {
 		protected static final int ax_ARM_AXIS=1;
 		
 		//Button Controls Intake System 
-		protected static final int angle_TRAP_DOOR=2;
+		protected static final int btn_ANGLE_TRAP_DOOR=2;
 		protected static final int ax_INTAKE_AXIS=3;
-		protected static final int incline_plane=4;
+		protected static final int btn_ANGLE_plane=4;
 
 		//controls
 		protected static final int DRIVE_CONTROLLER=1;
